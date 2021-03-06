ee709871ac64d2e279ca088f34458e31f184cf9f
Camila Cintioli <camila.cintioli@gmail.com>
2021-03-06 11:18:13 -0300

    Added fields in job form

--- online-job-exchange-frontend/src/components/forms/NewJobForm.tsx
+++ online-job-exchange-frontend/src/components/forms/NewJobForm.tsx
@@ -1,32 +1,59 @@
 import React from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form } from 'formik'
+
+import DateField from './fields/DateField'
+import OrganizationField from './fields/OrganizationField'
+import TextField from './fields/TextField'
+import TextAreaField from './fields/TextAreaField'
+
 import useI18n from '../../hooks/useI18n'
 
 export default function NewJobForm(): JSX.Element {
     const { t } = useI18n()
     return (
-        <Formik initialValues={{}} onSubmit={(values) => { }}>
+        <Formik initialValues={{
+            organization: "",
+            job: {
+                name: "",
+                description: "",
+                dateFrom: null,
+                dateTo: null,
+            }
+        }} onSubmit={(values) => { }}>
             <Form>
-                <label>
-                    {t("job.form.organization.label")}
-                    <Field name="organization" placeholder={t("job.form.organization.placeholder")} />
-                </label>
-                <label>
-                    {t("job.form.name.label")}
-                    <Field name="job.name" placeholder={t("job.form.name.placeholder")} />
-                </label>
-                <label>
-                    {t("job.form.description.label")}
-                    <Field name="job.description" placeholder={t("job.form.description.placeholder")} />
-                </label>
-                <label>
-                    {t("job.form.dateFrom.label")}
-                    <Field name="job.dateFrom" placeholder={t("job.form.dateFrom.placeholder")} />
-                </label>
-                <label>
-                    {t("job.form.dateTo.label")}
-                    <Field name="job.dateTo" placeholder={t("job.form.dateTo.placeholder")} />
-                </label>
+                <OrganizationField
+                    label="job.form.organization.label"
+                    id="organization"
+                    name="organization"
+                    placeholder="job.form.organization.placeholder" />
+
+                <TextField
+                    label="job.form.name.label"
+                    id="name"
+                    name="job.name"
+                    placeholder="job.form.name.placeholder"
+                />
+
+                <TextAreaField
+                    id="description"
+                    label="job.form.description.label"
+                    name="job.description"
+                    placeholder="job.form.description.placeholder"
+                />
+                
+                <DateField
+                    label="job.form.dateForm.label"
+                    id="dateFrom"
+                    name="job.dateFrom"
+                    placeholder="job.form.dateFrom.placeholder"
+                />
+
+                <DateField
+                    id="dateTo"
+                    label="job.form.dateTo.label"
+                    name="job.dateTo"
+                    placeholder="job.form.dateTo.placeholder"
+                />
             </Form>
         </Formik>
     )
---
+++ online-job-exchange-frontend/src/components/forms/fields/OrganizationField.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useField } from 'formik'
+import useI18n from '../../../hooks/useI18n'
+
+interface Props {
+    id: string
+    label: string
+    name: string
+    placeholder: string
+}
+
+export default function OrganizationField({
+    label, ...props
+}: Props): JSX.Element {
+    const { t } = useI18n();
+    const [field, meta, helpers] = useField<string>(props);
+
+    return (
+        <label>
+            {t(label)}
+            <input
+                {...field}
+                placeholder={t(props.placeholder)}
+            />
+            <input />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </label>
+    );
+}
+
+
