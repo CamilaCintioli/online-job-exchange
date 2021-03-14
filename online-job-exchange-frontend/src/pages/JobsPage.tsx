23653879c34f44cf4744feb9cbd5cb16ab1833f8
Camila Cintioli <camila.cintioli@gmail.com>
2021-03-14 17:43:34 -0300

    Jobs page now shows job detail

--- online-job-exchange-frontend/src/pages/JobsPage.tsx
+++ online-job-exchange-frontend/src/pages/JobsPage.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useCallback } from 'react'
-import useJobListing from '../hooks/useJobListing'
+import { useLocation } from 'react-router-dom'
+
 import SearchBar from '../components/SearchBar'
 import JobList from '../components/JobList'
+import JobDetail from '../components/JobDetail'
 
+import useJobListing from '../hooks/useJobListing'
+import useJobOfferDetail from '../hooks/useJobOfferDetail'
 
 export default function JobsPage(): JSX.Element {
     const [keyword, setKeyword] = useState("")
@@ -10,12 +14,22 @@ export default function JobsPage(): JSX.Element {
 
     const handleSearchSubmit = useCallback(({ keyword }) => {
         setKeyword(keyword)
-    }, [])
+    }, []);
+
+    const { hash } = useLocation()
+    const focusedOfferId = hash.slice(1)
+    const [jobOffer] = useJobOfferDetail(focusedOfferId)
     
     return (
         <>
            <SearchBar onSubmit={handleSearchSubmit} />
-           <JobList jobOffers={jobOffers}/>
+           <div style={{
+               display: "grid",
+               gridTemplateColumns: "1fr 1fr"
+           }}>
+            <JobList jobOffers={jobOffers}/>
+            <JobDetail jobOffer={jobOffer} />
+           </div>
         </>
     )
 }
