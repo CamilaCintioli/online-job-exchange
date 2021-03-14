6a18fd8b6e019f08c64c40a43c15f91c79bb1142
Camila Cintioli <camila.cintioli@gmail.com>
2021-03-14 17:41:56 -0300

    Remove types from job list component

--- online-job-exchange-frontend/src/components/JobList.tsx
+++ online-job-exchange-frontend/src/components/JobList.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
+import type JobOffer from '../types/JobOffer'
 
 import useI18n from '../hooks/useI18n'
 
-interface Job {
-    organization: {
-        id: string
-        name: string
-        logo: string
-    }
-    job: {
-        id: string
-        name: string
-        description: string
-        publishedAt: string
-    }
-}
-
 interface Props {
-    jobOffers: Job[]
+    jobOffers: JobOffer[]
 }
 
 export default function JobList({ jobOffers }: Props): JSX.Element {
-  const { t } = useI18n();
+    const { t } = useI18n();
 
     return (
         <ul>
@@ -29,9 +16,11 @@ export default function JobList({ jobOffers }: Props): JSX.Element {
                 jobOffers.map(jobOffer => (
                     <li>
                         <article>
-                            <img src={jobOffer.organization.logo} alt={t('jobOffer.organization.logo.alt', { organizationName: jobOffer.organization.name })} />
-                            <h4>{jobOffer.job.name}</h4>
-                            <h5>{jobOffer.organization.name}</h5>
+                            <a href={`#${jobOffer.id}`}>
+                                <img src={jobOffer.organization.logo} alt={t('jobOffer.organization.logo.alt', { organizationName: jobOffer.organization.name })} />
+                                <h5>{jobOffer.organization.name}</h5>
+                                <h4>{jobOffer.name}</h4>
+                            </a>
                         </article>
                     </li>
                 ))
