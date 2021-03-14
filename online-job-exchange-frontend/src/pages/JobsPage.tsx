f92ce94667f741fae85118a0f746068d920c51a9
Camila Cintioli <camila.cintioli@gmail.com>
2021-03-14 17:15:24 -0300

    Added jobs page

--- online-job-exchange-frontend/src/pages/JobsPage.tsx
+++ online-job-exchange-frontend/src/pages/JobsPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import useJobListing from '../hooks/useJobListing'
+import SearchBar from '../components/SearchBar'
+import JobList from '../components/JobList'
+
 
 export default function JobsPage(): JSX.Element {
+    const [keyword, setKeyword] = useState("")
+    const [jobOffers] = useJobListing(keyword)
+
+    const handleSearchSubmit = useCallback(({ keyword }) => {
+        setKeyword(keyword)
+    }, [])
+    
     return (
-        <div>Jobs</div>
+        <>
+           <SearchBar onSubmit={handleSearchSubmit} />
+           <JobList jobOffers={jobOffers}/>
+        </>
     )
 }
