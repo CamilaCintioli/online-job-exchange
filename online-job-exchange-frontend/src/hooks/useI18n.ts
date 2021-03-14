1cedbd9b1eb624ecaaf8f38181daa9f6649b0d7e
Camila Cintioli <camila.cintioli@gmail.com>
2021-03-14 17:15:59 -0300

    Added fillers to i18n hook

--- online-job-exchange-frontend/src/hooks/useI18n.ts
+++ online-job-exchange-frontend/src/hooks/useI18n.ts
@@ -1,7 +1,7 @@
 interface I18nHelpers {
-    t: (id: string) => string
+    t: (id: string, fillers?: Record<string, string>) => string
 }
 
 export default function useI18n(): I18nHelpers{
-    return { t: (id)=>id }
+    return { t: (id, fillers)=>id }
 }
